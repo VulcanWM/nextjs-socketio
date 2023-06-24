cbc4e54bb175460f7aecd7afd58b3a0257bd5b5b
VulcanWM <logmedha09@gmail.com>
2023-06-24 20:27:40 +0100

    added socket

---
+++ pages/api/socket.js
@@ -0,0 +1,22 @@
+import { Server } from 'socket.io'
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server, {
+      path: "/api/socket",
+    })
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('input-change', msg => {
+        socket.broadcast.emit('update-input', msg)
+      })
+    })
+  }
+  res.end()
+}
+
+export default SocketHandler
